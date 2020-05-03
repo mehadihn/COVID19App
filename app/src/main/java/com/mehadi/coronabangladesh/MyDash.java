e2580315b1350fbfaa90dae5d58a84cf3d8b83b8
Mehadi Hassan <mehadihassan116@gmail.com>
2020-05-03 13:23:46 +0600

    Corona Tips Scroll and Hospital

--- app/src/main/java/com/mehadi/coronabangladesh/MyDash.java
+++ app/src/main/java/com/mehadi/coronabangladesh/MyDash.java
@@ -6,18 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MyDash extends AppCompatActivity {
-    private LinearLayout newsLa;
-    private LinearLayout banglaAct;
-    private LinearLayout worldAct;
-    private LinearLayout hospital;
+    private RelativeLayout newsLa;
+    private RelativeLayout banglaAct;
+    private RelativeLayout worldAct;
+    private RelativeLayout hospital;
+    private RelativeLayout helpline;
+
+    private TextView greeting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_dash);
+
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+
+        greeting = findViewById(R.id.greeting);
+
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            greeting.setText("Good Morning");
+        }else if(timeOfDay >= 12 && timeOfDay < 16){
+            greeting.setText("Good Afternoon");
+        }else if(timeOfDay >= 16 && timeOfDay < 21){
+            greeting.setText("Good Evening");
+        }else if(timeOfDay >= 21 && timeOfDay < 24){
+            greeting.setText("Good Night");
+        }
+
+
+
+
+
+
+
+
+
 
         newsLa = findViewById(R.id.newsla);
         newsLa.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +83,16 @@ public class MyDash extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MyDash.this, hospital.class);
+                startActivity(i);
+            }
+        });
+
+
+        helpline = findViewById(R.id.helpline);
+        helpline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MyDash.this, helpline.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/mehadi/coronabangladesh/hospital.java
+++ app/src/main/java/com/mehadi/coronabangladesh/hospital.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 public class hospital extends AppCompatActivity {
-    ImageView kuwaitCall , dmcCall , KamlaCall;
+    ImageView kuwaitCall , dmcCall , KamlaCall , dhakamohan , mmh , Kamrangirchar , Aminbazar , National , Sheikh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,66 @@ public class hospital extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:0255007420"));
+                startActivity(intent);
+            }
+        });
+
+        dhakamohan = findViewById(R.id.dhakamohan);
+        dhakamohan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0257390860"));
+                startActivity(intent);
+            }
+        });
+
+        mmh = findViewById(R.id.mmh);
+        mmh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:029002012"));
+                startActivity(intent);
+            }
+        });
+
+        Kamrangirchar = findViewById(R.id.Kamrangirchar);
+        Kamrangirchar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01726321189"));
+                startActivity(intent);
+            }
+        });
+
+        Aminbazar = findViewById(R.id.Aminbazar);
+        Aminbazar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01712290100"));
+                startActivity(intent);
+            }
+        });
+
+        National = findViewById(R.id.National);
+        National.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:028811910"));
+                startActivity(intent);
+            }
+        });
+
+        Sheikh = findViewById(R.id.Sheikh);
+        Sheikh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:028811910"));
                 startActivity(intent);
             }
         });
